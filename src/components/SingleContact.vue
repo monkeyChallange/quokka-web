629fed0b77d82707b418a294c17a33654b8550e8
Almir Kapa <almir.kapa@gmail.com>
2021-01-28 18:55:54 +0100

    New logos, backgrounds

---
+++ src/components/SingleContact.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="contact-image">
+      <PictureFrame width="200" height="179.5688" />
+    </div>
+    <div class="contact-details">
+      <h3>{{ name }}</h3>
+      <ul>
+        <li>{{ position }}</li>
+        <li>{{ phone }}</li>
+        <li>
+          <a :href="'mailto:' + email">{{ email }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import PictureFrame from "@/components/PictureFrame.vue"
+
+export default {
+  components: { PictureFrame },
+  props: ["name", "position", "phone", "email"],
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.contact-details {
+  padding-left: 50px;
+}
+.contact-details h3 {
+  text-align: left;
+}
+.contact-details ul {
+  list-style-type: none;
+  margin: 0px;
+  padding: 0px;
+  line-height: 30px;
+  color: white;
+  text-align: left;
+  font-size: 20px;
+}
+</style>
---
+++ src/components/logos/LinkedInLogo.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    clip-rule="evenodd"
+    fill-rule="evenodd"
+    image-rendering="optimizeQuality"
+    shape-rendering="geometricPrecision"
+    text-rendering="geometricPrecision"
+    version="1.1"
+    viewBox="0 0 3334 3334"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:cc="http://creativecommons.org/ns#"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  >
+    <path
+      d="m1667 0c920 0 1667 746 1667 1667 0 920-746 1667-1667 1667-920 0-1667-746-1667-1667 0-920 746-1667 1667-1667zm-215 1336h342v175h5c48-86 164-175 338-175 361 0 428 225 428 517v596h-357v-528c0-126-3-288-186-288-186 0-214 137-214 279v537h-357v-1113zm-247-309c0 102-83 186-186 186-102 0-186-83-186-186 0-102 83-186 186-186 102 0 186 83 186 186zm-371 309h371v1113h-371z"
+      fill="#fff"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ["width", "height"],
+}
+</script>
