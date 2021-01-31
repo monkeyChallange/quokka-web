34354af10d9f7270b195797a2ed73d954db8c5ec
Almir Kapa <almir.kapa@gmail.com>
2021-01-31 21:54:03 +0100

    Added new incubatior link

---
+++ src/views/Incubator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page incubator">
+    <div class="content">
+      <h2>Incubator for start-ups!</h2>
+      <p>
+        In addition to offering engineering services to our clients, Quokka is
+        an incubator for start-ups. Our innovation hub gives our employees and
+        partners an opportunity to realize their business ideas. Within our
+        company group, we have experience from starting over 20 companies and we
+        have a genuin interest to invest in people and ideas we believe in.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.page.incubator {
+  background-image: url("~@/assets/backgrounds/quokka-pattern-2.png");
+}
+</style>
